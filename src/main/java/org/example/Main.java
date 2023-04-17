91e38a2e396ae690961517af7515685a704cafd2
fickler <2586844575@qq.com>
2023-04-17 23:00:05 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author dell
+ * @date 2023/4/17 22:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
